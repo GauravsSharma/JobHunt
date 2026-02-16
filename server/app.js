8cd03510d875ca9c2f0dcd6a66c2f3676e31eea9
Gaurav Sharma <gauravsharma16072001@gmail.com>
2026-02-16 17:06:32 +0530

    test

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ connectToDatabase();
 app.get("/",(req,res)=>{
     res.status(200).json({
         success:true,
-        message:"App is up and running v3."
+        message:"App is up and running"
     })
 })
 app.get("/health",(req,res)=>{
